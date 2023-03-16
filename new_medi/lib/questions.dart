84823b1d5b16670c2cda61d5fb37414a145561c0
pacmadhushan <chathuramadhushanpa@gmail.com>
2023-03-16 09:37:29 +0530

    data fetch questions

--- new_medi/lib/questions.dart
+++ new_medi/lib/questions.dart
@@ -1,6 +1,41 @@
 import 'package:flutter/material.dart';
-
 import 'trakOrder.dart';
+
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<Album> fetchAlbum() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/6'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class Album {
+  final String password;
+  final String username;
+
+  const Album({
+    required this.password,
+    required this.username,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      password: json['password'],
+      username: json['username'],
+    );
+  }
+}
 
 class Questions extends StatefulWidget {
   const Questions({super.key});
